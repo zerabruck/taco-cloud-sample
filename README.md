cdd701e8dea208356b60db21f80557a4f8e71c1c
Betsegaw Lemma Amersho <betsegawlemma@gmail.com>
2021-12-22 12:48:38 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sample Starting Taco  Cloud App
---
+++ src/test/java/com/betsegaw/tacos/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betsegaw.tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
